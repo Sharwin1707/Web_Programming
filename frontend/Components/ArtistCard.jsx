5730e86b600ea925d698d651ecfb865cb297b656
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-04-24 19:32:50 +0800

    modify link ArtistCard

--- frontend/Components/ArtistCard.jsx
+++ frontend/Components/ArtistCard.jsx
@@ -5,7 +5,7 @@ const ArtistCard = ({id,image, name}) => {
   return (
         <div className='bg-[#222222] w-[300px] h-[400px] p-2'>
             
-            <Link to={`/artist`}>
+            <Link to={`/artistprofile/${id}`}>
             <div className='h-[300px] overflow-hidden rounded-md'>      
                 <img className='w-full h-full object-cover  rounded-md' src={image} alt="" />
             </div>
