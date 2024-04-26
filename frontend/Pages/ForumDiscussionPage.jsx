dafeb24f949181cad3832d38a731f9f7e562f574
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-04-26 12:09:10 +0800

    forum page

--- frontend/Pages/ForumDiscussionPage.jsx
+++ frontend/Pages/ForumDiscussionPage.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faArrowLeft,  faMagnifyingGlass, faUserCircle, faHeart, faMessage, faReply } from '@fortawesome/free-solid-svg-icons'
 import { Link, useParams } from 'react-router-dom'
+import FroumChatContainer from '../Components/FroumChatContainer'
 
 const ForumDiscussionPage = () => {
 
     const {id} = useParams()
-    const title = id.replace('%',' ');
+    const topicTitle = id.replace('%',' ');
+    const [openModal, setOpenModal] = useState(false)
+    const [forumDiscussion, setDiscussion] = useState([])
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const [isLiked, setLike] = useState(false)
+    
+    const likeClicked = () => {
+        setLike(!isLiked)
+    }
+
+    const createPostModal = () => {
+        setOpenModal(!openModal)
+    }
+
+    const addPost = () => {
+        setDiscussion(prevTopic => [...prevTopic, { topic: title, content: content }]);
+        setTitle('');
+        setContent('');
+        setOpenModal(false); // Close the modal after adding the post
+    }
+    
+    
+    useEffect(() => {
+        const reverse = forumDiscussion.reverse();
+        setDiscussion(reverse)
+    },[forumDiscussion])
 
 
   return (
@@ -24,7 +52,7 @@ const ForumDiscussionPage = () => {
 
         
       <div className='py-10'>
-        <h1 className='text-2xl'>{title}</h1>
+        <h1 className='text-2xl'>{topicTitle}</h1>
         
         {/*-------------------Discussion topic -------------*/}
         <div className='my-10 py-4 border-y '> 
@@ -35,8 +63,8 @@ const ForumDiscussionPage = () => {
             </div>
             <p className='py-4 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In maxime non, illum dolorum sint quaerat esse sequi? Possimus minus sint eaque debitis quidem, reprehenderit dolorum eos. Distinctio, ab. Similique, odio.</p>
             <div className='flex gap-4 items-center'>
-                <FontAwesomeIcon className='w-6 h-6' icon={faHeart}/>
-                <FontAwesomeIcon className='w-6 h-6' icon={faMessage}/>
+                <FontAwesomeIcon className='w-6 h-6' icon={faHeart} color={isLiked ? 'red': ''} onClick={likeClicked}/>
+                <FontAwesomeIcon className='w-6 h-6' icon={faMessage} onClick={createPostModal}/>
                 <FontAwesomeIcon className='w-6 h-6' icon={faReply}/>
             </div>
         </div>
@@ -45,36 +73,34 @@ const ForumDiscussionPage = () => {
         {/*---------------------- Reply------------------------------------ */}
 
         <div className='ml-8'>
-            <div className='my-10 py-4 bg-blue-gray-300 p-3 rounded-md'> 
 
-                <div className='flex gap-3'>
-                    <FontAwesomeIcon icon={faUserCircle} size='2x'/>
-                    <h1>Ali</h1>
-                </div>
-                <p className='py-4 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In maxime non, illum dolorum sint quaerat esse sequi? Possimus minus sint eaque debitis quidem, reprehenderit dolorum eos. Distinctio, ab. Similique, odio.</p>
-                <div className='flex gap-4 items-center'>
-                    <FontAwesomeIcon className='w-6 h-6' icon={faHeart}/>
-                    <FontAwesomeIcon className='w-6 h-6' icon={faMessage}/>
-                    <FontAwesomeIcon className='w-6 h-6' icon={faReply}/>
-                </div>
-            </div>
+            {forumDiscussion.reverse().map(discussions => (
+                <FroumChatContainer reply={discussions.content}/>
+            ))}
 
-            <div className='my-10 py-4 bg-blue-gray-300 p-3 rounded-md'> 
+            <FroumChatContainer reply={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. In maxime non, illum dolorum sint quaerat esse sequi? Possimus minus sint eaque debitis quidem, reprehenderit dolorum eos. Distinctio, ab. Similique, odio.'}/>
+            <FroumChatContainer reply={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. In maxime non, illum dolorum sint quaerat esse sequi? Possimus minus sint eaque debitis quidem, reprehenderit dolorum eos. Distinctio, ab. Similique, odio.'}/>
+            
+            
 
-                <div className='flex gap-3'>
-                    <FontAwesomeIcon icon={faUserCircle} size='2x'/>
-                    <h1>Abu</h1>
-                </div>
-                <p className='py-4 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In maxime non, illum dolorum sint quaerat esse sequi? Possimus minus sint eaque debitis quidem, reprehenderit dolorum eos. Distinctio, ab. Similique, odio.</p>
-                <div className='flex gap-4 items-center'>
-                    <FontAwesomeIcon className='w-6 h-6' icon={faHeart}/>
-                    <FontAwesomeIcon className='w-6 h-6' icon={faMessage}/>
-                    <FontAwesomeIcon className='w-6 h-6' icon={faReply}/>
-                </div>
-            </div>
         </div>
         
       </div>
+
+
+      {openModal ? <div className='absolute w-screen h-screen top-0 left-0 backdrop-blur-sm flex justify-center items-center'>
+          <div className=' w-[800px] h-[600px] bg-white rounded-md text-black'>
+              <div className='flex flex-col px-6 py-3'>
+                <label htmlFor="">Reply:</label>
+                <textarea onChange={event => setContent(event.target.value)} className='border border-gray-600 px-4 py-2' name="" id="" cols="30" rows="15"></textarea>
+              </div>
+
+              <div className='px-6 flex gap-10'>
+                <button onClick={addPost} className='px-4 py-2 border-4 border-red-400 rounded-md'>Create</button>
+                <button onClick={createPostModal} className='px-5 py-3 rounded-md bg-red-400 text-white'>Cancel</button>
+              </div>
+          </div>
+      </div> : ''}
 
     </div>
   )
