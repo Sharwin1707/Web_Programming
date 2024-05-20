586bd4660457fe7aa53504271245638bcf42fdc1
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-05-20 12:22:41 +0800

    Database connection setup

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import DatabaseConnection from './config/db.js';
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express();
 
-
+DatabaseConnection()
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`)
