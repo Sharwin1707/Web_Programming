1b70bbf68b1ceef7dec1e60565dc6d17c77c0933
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-05-20 13:52:47 +0800

    User model and user routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import DatabaseConnection from './config/db.js';
+import { router as UserRoute } from './routes/UserRoute.js';
+
+
 dotenv.config()
-
-const PORT = process.env.PORT || 5000
-
-const app = express();
-
 DatabaseConnection()
+
+const PORT = process.env.PORT || 5000 
+const app = express();
+app.use(express.json());
+
+
+app.use('/users', UserRoute);
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`)
